cc3f1f7a76aabc177788f8746a2ec42f884773e5
dhasane <danihas@live.com>
2022-01-27 22:13:04 -0500

    Fix client cli interface

--- src/cliente.rs
+++ src/cliente.rs
@@ -1,30 +1,70 @@
 mod communication;
 use std::env;
 
+use clap::{App, Arg};
 use communication::connection::Connection;
 
+const DISPATCHER_IP: &str = "dispatcher ip";
+const DISPATCHER_PORT: &str = "dispatcher port";
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let cli_matches = App::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .about(clap::crate_description!())
+        .arg(
+            Arg::with_name(DISPATCHER_IP)
+                .help("dispatcher ip")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name(DISPATCHER_PORT)
+                .help("dispatcher port")
+                .long("port")
+                .short("p")
+                .default_value("8080"),
+        )
+        .arg(
+            Arg::with_name("filename")
+                .help("filename")
+                .long("filename")
+                .short("f")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("send")
+                .help("send")
+                .long("send")
+                .short("s")
+                .takes_value(false),
+        )
+        .setting(clap::AppSettings::ArgRequiredElseHelp)
+        .setting(clap::AppSettings::VersionlessSubcommands)
+        .get_matches();
 
-    if args.len() < 3 {
-        println!("Se debe especificar [ip dispatcher] [puerto dispatcher] [archivo]");
-        return;
+    let con = Connection {
+        ip: cli_matches.value_of(DISPATCHER_IP).unwrap().to_owned(),
+        port: cli_matches.value_of(DISPATCHER_PORT).unwrap().to_owned(),
+    };
+
+    let filename: String = cli_matches.value_of("filename").unwrap().to_owned();
+
+    if cli_matches.is_present("send") {
+        send(&con, filename);
+    } else {
+        get(&con, filename);
     }
+}
 
+fn send(con: &Connection, filename: String) {
+    println!("{}", con.put_file(&filename, 4).unwrap());
+}
+
+fn get(con: &Connection, filename: String) {
     let dir = "archivos-cliente".to_string();
 
     std::fs::create_dir_all(&dir).unwrap();
 
-    let con = Connection {
-        ip: args[1].to_owned(),
-        port: args[2].to_owned(),
-    };
-
-    let filename: String = args[3].to_owned();
-
-    // println!("{}", con.get_file(filename, dir).unwrap());
-
-    println!("{}", con.put_file(&filename, 4).unwrap());
-
-    // TODO: poner para que se pueda tener una opcion para elegir entre cargar y descargar
+    println!("{}", con.get_file(&filename, &dir).unwrap());
 }
